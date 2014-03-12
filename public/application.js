02cd52ce0a39dbfa63324e9f497f25f23d357cb7
Chris Kim <chris@wundercode.net>
2014-03-11 19:52:31 -0700

    When a request hits the server, I can recompile the scss to css in the public directory to serve as a response.

--- public/application.js
+++
@@ -1,8 +0,0 @@
-var VideoProject = function () {
-  var _this = this;
-
-  _this.name = "Video"
-  _this.shout = function () {
-    console.log(_this.name);
-  }
-}
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
-var app = express();
-var fs = require('fs');
+var app     = express();
+var sass    = require('node-sass');
+var fs      = require('fs');
+var path    = require('path');
+
+
+app.use(sass.middleware({
+    src: path.join(".", "stylesheets")
+  , dest: path.join(".", "public")
+  , debug: true
+}));
 
 app.use(express.static('public'));
 
